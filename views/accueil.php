c0d29f6dc1d7c860d253aa92d1fac045bc27147b
Abdulrazzak1995 <59529477+Abdulrazzak1995@users.noreply.github.com>
2020-09-15 23:03:41 +0200

    Update accueil.php

--- views/accueil.php
+++ views/accueil.php
@@ -1,5 +1,5 @@
 <div id="contenu">
 	<h2>Accueil</h2>
-	<p>Bienvenue aux séances d'exercices consacrées à élaborer un site Internet en PHP selon une architecture didactique MVC OO.</p>
+	<p>Site des Bonnes Nouvelles on Azure</p>
 	<p>Jean-Luc Collinet.</p>
 </div><!-- #contenu -->
